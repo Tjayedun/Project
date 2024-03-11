696ec55af9d7fba0398cf4a8a66ffc6040eb6179
Tjayedun <143630136+Tjayedun@users.noreply.github.com>
2024-03-11 16:14:03 +0000

    3/11

---
+++ script.js
@@ -0,0 +1,8 @@
+let American = document.querySelector(".American");
+let German = document.querySelector(".German");
+let Japan = document.querySelector(".Japan");
+let Italy = document.querySelector(".Italy");
+
+American.addEventListener("click", function() {
+    console.log("click");
+}
